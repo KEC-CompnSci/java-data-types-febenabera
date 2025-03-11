a173dc50a70daece12fa305d9aa6c35c5345c161
febenabera <25aberaf@retsdstudent.ca>
2025-03-11 11:24:39 -0500

    Update Assignment.java

--- src/main/java/Assignment.java
+++ src/main/java/Assignment.java
@@ -1,21 +1,28 @@
 public class Assignment {
     // TODO: Create a String variable named myString and assign it any value
     public String myString;
+    myString = "hello";
 
     // TODO: Create an integer variable named myInt and assign it any value
     public int myInt;
+myInt = 54;
 
     // TODO: Create a double variable named myDouble and assign it any value
     public double myDouble;
+myDouble = 3.14;
 
     // TODO: Create a boolean variable named myBoolean and assign it any value
     public boolean myBoolean;
+myBoolean = true;
+
 
     // TODO: Create a String array named myStringArray with at least 3 elements
     public String[] myStringArray;
+myStringArray = { "red", "orange", "yellow"};
 
     // TODO: Create an integer array named myIntArray with at least 3 elements
     public int[] myIntArray;
+myIntArray = { 1, 2, 3 };
 
     public Assignment() {
         // Students will initialize their variables here
